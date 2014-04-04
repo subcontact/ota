ecf92b6af9197bc2f26031c296abe84cc26217c1
tyronne <tyronne@tyronnes-iMac.local>
2014-04-04 20:06:44 +1100

    update

---
+++ alias.sh
@@ -0,0 +1,4 @@
+
+alias orun="restart --watch *.js *.template --exec node --harmony server.js --builds ~/code/jobs --nocache true"
+alias qgen="./generate.py clean && ./generate.py distclean && ./generate.py && ./generate.py build"
+
